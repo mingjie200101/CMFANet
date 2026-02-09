d71df3c4f52f6f73d82f8b0f572a463ad3c391b3
mingjie200101 <129475074+mingjie200101@users.noreply.github.com>
2026-02-09 13:21:03 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,5 @@
-import sys
-sys.path.insert(0, "/home/jiayuan/ultralytics-main/ultralytics")
-
 from ultralytics import YOLO
 
 
-number = 3 #input how many tasks in your work
-model = YOLO('/media/lwb/92781CDD781CC1C1/lhr/yolomv1/runs/multi/lm-yolo-manet+s-s/weights/best.pt')  # Validate the model
-model.predict(source='/media/lwb/92781CDD781CC1C1/lhr/yolomv1/test2', imgsz=(384,672), device=[0],name='vis', save=True, conf=0.25, iou=0.45, show_labels=False)
+model = YOLO('/data/workspace/lhr/CMFANet/CMFANet/weights/best.pt',task='multi')  # Validate the model
+model.predict(source='test.jpg', imgsz=(640,640), device=[0],name='pre', save=True, conf=0.25, iou=0.45, show_labels=False)
